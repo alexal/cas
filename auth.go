75494ee1e7155f68b77064efcbec1e4b91433fb9
Alex Rodin <alex.rodin@sas.com>
2020-05-26 16:13:10 -0400

    New auth.go file
    I've moved all authentication functions to auth.go fil

---
+++ auth.go
@@ -0,0 +1,63 @@
+package cas
+
+import (
+	"crypto/tls"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
+)
+
+const (
+	HTTPClientTimeout = 15 * time.Second
+)
+
+type Client struct {
+	http     *http.Client
+	baseURL  string
+	userName string
+	password string
+	debugLog *log.Logger
+}
+
+func NewBasicAuthClient(url, username, password string) *Client {
+	//TODO: For development purposes only.
+	customTransport := http.DefaultTransport.(*http.Transport).Clone()
+	customTransport.TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
+	return &Client{
+		baseURL:  url,
+		userName: username,
+		password: password,
+		http: &http.Client{
+			Timeout:   HTTPClientTimeout,
+			Transport: customTransport,
+		},
+	}
+}
+
+func (c *Client) doRequest(req *http.Request) ([]byte, error) {
+	req.SetBasicAuth(c.userName, c.password)
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Accept", "application/json")
+	resp, err := c.http.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	responseBody, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if len(responseBody) == 0 {
+		return nil, fmt.Errorf("HTTP %d: %s (body empty)", resp.StatusCode, http.StatusText(resp.StatusCode))
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("%s", responseBody)
+	}
+
+	return responseBody, nil
+}
